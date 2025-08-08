83023f23ed33311b2841d67e28683a4ce35a6f7d
Fatai Ayeloja <fayeloja@gmail.com>
2025-08-08 16:59:06 +0100

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 class="text-3xl text-center mt-10">
+          Test Assessment
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
